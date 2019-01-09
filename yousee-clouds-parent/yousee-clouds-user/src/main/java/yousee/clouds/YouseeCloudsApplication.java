3389ec074c678db16743b8eb12a19d0463587ff3
spe <1335666107@qq.com>
2019-01-09 21:51:28 +0800

    解决持久层报错问题

--- yousee-clouds-parent/yousee-clouds-user/src/main/java/yousee/clouds/YouseeCloudsApplication.java
+++ yousee-clouds-parent/yousee-clouds-user/src/main/java/yousee/clouds/YouseeCloudsApplication.java
@@ -1,10 +1,9 @@
-package yousee;
+package yousee.clouds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class YouseeCloudsApplication {
 
     public static void main(String[] args) {
