0c6cfaab67d0e445ea9cd8894a40f81cebcb49fa
spe <1335666107@qq.com>
2019-01-09 15:36:01 +0800

    初始eureka服务，修改pom包的继承关系

--- yousee-clouds-parent/yousee-clouds-eureka/src/main/java/yousee/clouds/YouseeCloudsApplication.java
+++ yousee-clouds-parent/yousee-clouds-eureka/src/main/java/yousee/clouds/YouseeCloudsApplication.java
@@ -2,8 +2,10 @@ package yousee.clouds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class YouseeCloudsApplication {
 
     public static void main(String[] args) {
