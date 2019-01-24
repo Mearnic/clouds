7e0d572def450be9391718f99bd1c36b35b493a4
spe <1335666107@qq.com>
2019-01-24 17:44:07 +0800

    解决springcloud 与springboot 2.1.X版本不兼容问题

--- yousee-clouds-parent/yousee-clouds-turbine/src/main/java/yousee/clouds/TurbineCloudsApplication.java
+++ yousee-clouds-parent/yousee-clouds-turbine/src/main/java/yousee/clouds/TurbineCloudsApplication.java
@@ -2,11 +2,9 @@ package yousee.clouds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
-//import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
-//@EnableTurbineStream
-@EnableTurbine
+@EnableTurbineStream
 @SpringBootApplication
 public class TurbineCloudsApplication {
 
