e8b850e4ddf99ba590d3d759ec54ca351e3e2582
spe <1335666107@qq.com>
2019-01-24 15:10:30 +0800

    监控系统组合

--- yousee-clouds-parent/yousee-clouds-video/src/main/java/yousee/clouds/video/core/controller/UserController.java
+++ yousee-clouds-parent/yousee-clouds-video/src/main/java/yousee/clouds/video/core/controller/UserController.java
@@ -1,5 +1,6 @@
 package yousee.clouds.video.core.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class UserController {
     @Resource
     private UserFeignService userFeignService;
 
+    @HystrixCommand
     @GetMapping(value = "/queryUserList")
     public R queryUserList(@RequestBody Map<String, Object> paramter) {
         return userFeignService.queryUserList(paramter);
--- yousee-clouds-parent/yousee-clouds-video/src/main/java/yousee/clouds/video/core/feign/UserFeignService.java
+++ yousee-clouds-parent/yousee-clouds-video/src/main/java/yousee/clouds/video/core/feign/UserFeignService.java
@@ -9,9 +9,9 @@ import yousee.clouds.video.system.feign.FeignConfiguration;
 
 import java.util.Map;
 
-@FeignClient(name = "yousee-clouds-user", configuration = FeignConfiguration.class, fallback = UserFeignFallbackFactory.class)
+@FeignClient(name = "yousee-clouds-user", configuration = FeignConfiguration.class, fallbackFactory = UserFeignFallbackFactory.class)
 public interface UserFeignService {
 
-    @RequestLine("POST /queryUserList")
+    @RequestLine("POST /user/queryUserList")
     public R queryUserList(@RequestBody Map<String, Object> paramter);
 }
