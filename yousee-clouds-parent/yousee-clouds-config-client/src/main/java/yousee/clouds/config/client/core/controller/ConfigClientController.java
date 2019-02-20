b61991e73e403c42739b06fa1b425c5f10cd0cf9
qiu hao yang <1335666107@qq.com>
2019-02-20 19:01:11 +0800

    解决动态刷新配置文件的问题

--- yousee-clouds-parent/yousee-clouds-config-client/src/main/java/yousee/clouds/config/client/core/controller/ConfigClientController.java
+++ yousee-clouds-parent/yousee-clouds-config-client/src/main/java/yousee/clouds/config/client/core/controller/ConfigClientController.java
@@ -2,6 +2,7 @@ package yousee.clouds.config.client.core.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import yousee.clouds.common.core.bean.R;
 
 @RestController(value = "configClientController")
 @RequestMapping("/config/client")
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${profile}")
